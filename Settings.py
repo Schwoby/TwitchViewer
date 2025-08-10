d3bc49a18777aa395d6ba4c2475677af1cd64304
Schwoby <65581397+Schwoby@users.noreply.github.com>
2025-08-09 22:07:16 -0500

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -3,7 +3,7 @@ TWITCH_API_URL = 'https://api.twitch.tv/helix/streams'
 
 STREAMER_ROTATE_FILE = "TwitchStreamers.csv"  # File containing the list of usernames to check
 ACTIVE_STREAMER_FILE = "LiveStreamers.csv"  # File containing the list of active streamers
-ROTATE_STREAMER_FILE = "ViewingStreamers.csv"  # File containing the list of live streamers to rotate though
+ROTATE_STREAMER_FILE = "ViewedStreamers.csv"  # File containing the list of live streamers to rotate though
 
 SCREEN_SAFE_ZONE = "287" # zone is the right side of screen in pixels
 SAFE_ZONE_RECHECK = "30" # zone recheck is how long the script waits after a safe zone detection before checking again
